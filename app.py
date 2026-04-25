7082893168a62deef228ac7a25a755dd9771ea24
Asasaafi <annisafitriana04@gmail.com>
2026-04-25 10:26:49 +0700

    update logic

--- app.py
+++ app.py
@@ -18,9 +18,12 @@ data = {
 
 df = pd.DataFrame(data)
 
-X = df[['process_1', 'process_2']]
-y = df['process_3']
+df['total'] = df['process_1'] + df['process_2'] + df['process_3']
 
+X = df[['process_1', 'process_2']]
+y = df['total']
+
+# Train
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
@@ -28,12 +31,11 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# evaluasi
+    # Eval
 y_pred = model.predict(X_test)
 mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-# Model
 st.subheader("📊 Model Evaluation")
 col1, col2 = st.columns(2)
 col1.metric("MAE", f"{mae:.2f}")
@@ -54,8 +56,11 @@ if st.button("Run Prediction"):
         'process_2': [p2]
     })
 
-    predicted_p3 = model.predict(input_data)[0]
-    total = p1 + p2 + predicted_p3
+    # TOTAL DULU
+    total = model.predict(input_data)[0]
+
+    # BARU HITUNG P3
+    predicted_p3 = total - p1 - p2
 
     # Output
     st.subheader("📈 Prediction Result")
@@ -75,26 +80,20 @@ if st.button("Run Prediction"):
     st.warning(f"🚨 Bottleneck: {bottleneck}")
 
     # Insight
-    st.subheader("🧠 AI Insight")
+    st.subheader("🧠 Insight")
     st.write(f"""
     The system identifies **{bottleneck}** as the most dominant stage in the workflow.  
     Reducing delay in this stage could significantly improve overall lead time.
     """)
 
-    #Recomend
-    st.subheader("🚀 AI Recommendation")
+    # Recommendation
+    st.subheader("🚀 Recommendation")
 
     if bottleneck == "Order → Warehouse":
-        st.write("""
-        Improve coordination and speed up item retrieval from warehouse systems.
-        """)
+        st.write("Improve coordination and speed up item retrieval from warehouse systems.")
 
     elif bottleneck == "Warehouse → Packing":
-        st.write("""
-        Optimize warehouse flow and improve material handling efficiency.
-        """)
+        st.write("Optimize warehouse flow and improve material handling efficiency.")
 
     else:
-        st.write("""
-        Increase manpower or improve packing efficiency to reduce processing time.
-        """)
+        st.write("Increase manpower or improve packing efficiency to reduce processing time.")
